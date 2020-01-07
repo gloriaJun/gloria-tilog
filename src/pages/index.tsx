46ebf9f602b08ae606f0cc0ec915cc811c353bec
gloriaJun <pureainu@gmail.com>
2020-01-08 01:10:31 +0900

    env: modify siteMetadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,5 @@
-import React from "react"
-import { Link } from "gatsby"
-import styled from "styled-components";
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage: React.FC = () => <>Test</>;
 
-const Container = styled.div`
-  margin-bottom: 1.45rem;
-  max-width: 300px;
-  border: 3px solid blue;
-`
-
-const IndexPage: React.FC = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Container>
-      <Image />
-    </Container>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default IndexPage;
