f06b297b438fb55b3e77cfb56d6d847c4b0a6537
gloriaJun <pureainu@gmail.com>
2020-01-08 01:13:27 +0900

    feature: add header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const IndexPage: React.FC = () => <>Test</>;
+import Header from 'components/header';
+
+import GlobalStyle from 'styles/GlobalStyle';
+
+const IndexPage: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Header title={'L.E.T'} />
+
+      <h1>Hello, World!!!</h1>
+      <h2>This is message.</h2>
+    </>
+  );
+};
 
 export default IndexPage;
