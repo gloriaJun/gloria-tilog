da15a0e63021c90ea6a9492935d2e074db6b9306
gloriaJun <pureainu@gmail.com>
2020-02-12 00:25:28 +0900

    env: configure typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,4 @@
 module.exports = {
-  stories: ['../src/**/*.stories.[tj]s'],
-  // stories: ['../src/**/*.stories.[tj]sx?'],
+  stories: ['../src/**/*.stories.[tj]sx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
 };
