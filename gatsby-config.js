01eee8cf030c792fd2cf43949cf5e1e09a8fb3c5
gloria <pureainu@gmail.com>
2020-02-12 23:37:27 +0900

    env: configure emotion and translate from styled-component (#39)

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     'gatsby-plugin-typescript',
-    'gatsby-plugin-styled-components',
+    'gatsby-plugin-emotion',
   ],
 };
