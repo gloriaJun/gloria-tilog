6215a96413e6c332440bb7ad373ad031392d3918
gloriaJun <pureainu@gmail.com>
2020-07-02 00:39:54 +0900

    chore: modify jest configuration

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,10 @@ module.exports = {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
     '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx',
-    '^.+\\.(jsx?|tsx?)$': `<rootDir>/jest-preprocess.js`,
+    '^.+\\.[jt]sx?$': `<rootDir>/jest-preprocess.js`,
   },
   // testRegex: ['(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$'],
-  testRegex: ['((\\.|/)(test|spec))\\.tsx?$'],
+  testRegex: ['((\\.|/)(test|spec))\\.([tj]sx?)$'],
   // moduleDirectories: ['node_modules', __dirname],
   moduleNameMapper: {
     '\\.svg': '<rootDir>/__mocks__/svgr-mock.ts',
@@ -18,19 +18,21 @@ module.exports = {
     '^components/(.*)$': '<rootDir>/src/components/$1',
     '^styles/(.*)$': '<rootDir>/src/styles/$1',
     '^icons/(.*)$': '<rootDir>/src/icons/$1',
+    '^mocks$': '<rootDir>/__mocks__/$1',
   },
-  testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`, `\\.svg`],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: [`node_modules`, `.cache`, `public`],
+  // Gatsby includes un-transpiled ES6 code. Exclude the gatsby module.
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`, `\\.svg`],
+  globals: {
+    __PATH_PREFIX__: '',
+    __BASE_PATH__: '',
+  },
+  testURL: `http://localhost`,
   setupFiles: ['<rootDir>/src/__tests__/loadershim.ts'],
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup-tests.ts'],
   // 테스트 전에 실행되는 global setup 모듈을 지정할 수 있다.
   // globalSetup: "./src/test/setup.ts",
   // 테스트가 끝난 후에 실행되는 global teardown 모듈을 지정할 수 있다.
   // globalTeardown: "./src/test/teardown.ts",
-  globals: {
-    __PATH_PREFIX__: '',
-    __BASE_PATH__: '',
-  },
-  testURL: `http://localhost`,
 };
